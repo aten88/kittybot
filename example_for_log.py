02d5b2e26b66c034ad0fcf93463ea98d0f7cbb4f
Алексей Тен <virtuoso_ten@mail.ru>
2023-02-15 23:33:14 +0300

    Added logs

---
+++ example_for_log.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+logging.debug('123')  # Когда нужна отладочная информация
+logging.info('Сообщение отправлено')  # Когда нужна дополнительная информация
+logging.warning('Большая нагрузка!')  # Когда что-то идёт не так, но работает
+logging.error('Бот не смог отправить сообщение')  # Когда что-то сломалось
+logging.critical('Всё упало! Зовите админа!1!111')  # Когда всё совсем плохо
